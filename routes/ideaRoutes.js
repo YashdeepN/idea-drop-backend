74571836ed87822106886f9c685653d2db27c3cd
YashdeepN <yashdeepneema@hotmail.com>
2025-08-19 07:10:55 -0500

    setup custom  error handler

---
+++ routes/ideaRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const ideas = [
+    { id: 1, title: "Idea 1", description: "This is idea 1" },
+    { id: 2, title: "Idea 2", description: "This is idea 2" },
+    { id: 3, title: "Idea 3", description: "This is idea 3" },
+  ];
+
+  res.status(400);
+  throw new Error("This is an error and it has worked !");
+
+  res.json(ideas);
+});
+
+router.post("/", (req, res) => {
+  const { title, description } = req.body;
+  console.log(description);
+  res.send(title);
+});
+
+export default router;
