e23dcac77b18444fab4f2a43ad8512a3e7b1deff
YashdeepN <yashdeepneema@hotmail.com>
2025-08-21 23:38:23 -0500

    set refresh token setting sameSite to lax

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -174,7 +174,7 @@ router.post("/logout", (req, res) => {
   res.clearCookie("refreshToken", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   });
 
   res.status(200).json({ message: "Loggged out successfully" });
